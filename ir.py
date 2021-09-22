13391fcbecc734cc50f3e63357362c3249134f6c
Anthony Kritikos <apkritikos@gmail.com>
2021-09-22 20:40:25 +0100

    Added current time to garage status print statement.

--- ir.py
+++ ir.py
@@ -3,6 +3,9 @@ import RPi.GPIO as GPIO
 # Prints info of GPIO device.
 print(GPIO.RPI_INFO)
 
+# Imports library for displaying time..
+import datetime
+
 # Imports time library for sleep.
 import time
 
@@ -19,14 +22,15 @@ GPIO.setup(pir_port,  GPIO.IN)
 # Permanent loop unitil user cancels with Ctrl C.
 try:
     while (True):
+        currentTime = datetime.datetime.now()
         # If the input is 0, sensor detects an obstacle.
         # Need to look into the reasoning behind this - 
         # Seems as though it should be the other way around.
         if GPIO.input(pir_port) == 0:
-            print("Garage Door is Closed.")
+            print("Garage Door is Closed at " + currentTime.strftime("%H:%M:%S %p on %m-%d-%Y"))
         else:
             # If not 0, then sensor detects nothing. 
-            print("Garage Door is Open.")
+            print("Garage Door is Open at " + currentTime.strftime("%I:%M:%S %p on %m-%d-%Y"))
         # Waits for 1 second for sensor input. 
         time.sleep(1)
 # If user presses Ctrl C, clean GPIO and exit. 
