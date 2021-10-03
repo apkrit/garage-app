7a647088d28549a65b9568a91ac29b2a098fc762
Anthony Kritikos <apkritikos@gmail.com>
2021-10-03 20:40:54 +0100

    Updated ir.py to check changes in garage status and accordingly save status to sensorlog database when a change is detected. Next step is to track how long the garage door has been open.

--- ir.py
+++ ir.py
@@ -15,8 +15,7 @@ import time
 
 # Creates database connection
 conn = psycopg2.connect('dbname=garagedb')
-curr = conn.cursor()
-
+cursor = conn.cursor()
 
 def get_all_sensor_input():
     query = """
@@ -25,8 +24,18 @@ def get_all_sensor_input():
     FROM
         sensorlog
     """
-    curr.execute(query)
-    return curr.fetchall()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+def add_sensor_record(status, time):
+    query = """
+    INSERT INTO
+        sensorlog
+    VALUES
+        (%s, %s)
+    """
+    values = (status, time)
+    cursor.execute(query, values)
 
 sensor_results = get_all_sensor_input()
 
@@ -49,24 +58,42 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pir_port,  GPIO.IN)
 
 # Permanent loop unitil user cancels with Ctrl C.
+lastStatus = None
 print('---------------------------------------------')
 print('Begining display of live sensor results:')
 try:
     while (True):
         currentTime = datetime.datetime.now()
-        # If the input is 0, sensor detects an obstacle.
+        currentTime = currentTime.strftime("%I:%M:%S %p on %m-%d-%Y")
+        garageStatus = None
+        # If the input is 0, sensor detects an obstacle, so door is closed.
         # Need to look into the reasoning behind this - 
         # Seems as though it should be the other way around.
         if GPIO.input(pir_port) == 0:
-            print("Garage Door is Closed at " + currentTime.strftime("%I:%M:%S %p on %m-%d-%Y"))
+            garageStatus = 'Closed'
         else:
-            # If not 0, then sensor detects nothing. 
-            print("Garage Door is Open at " + currentTime.strftime("%I:%M:%S %p on %m-%d-%Y"))
+            # If not 0, then sensor detects nothing, so door is open.
+            garageStatus = 'Open'
+        # Store initial startup value to sensorlog db
+        if lastStatus == None:
+            print('Initial state saved to sensorlog db.')
+            add_sensor_record(garageStatus, currentTime)
+            conn.commit()
+        if garageStatus != lastStatus and lastStatus != None:
+            print('Garage status has changed!')
+            add_sensor_record(garageStatus, currentTime)
+            conn.commit()
+        lastStatus = garageStatus
+        garageStatusReadout = f"Garage door is {garageStatus} at {currentTime}"
+        print(garageStatusReadout)
         # Waits for 1 second for sensor input. 
         time.sleep(1)
 # If user presses Ctrl C, clean GPIO and exit. 
 except KeyboardInterrupt:
     GPIO.cleanup()
+finally:
+    cursor.close()
+    conn.close()
 print("Exiting")
 print('---------------------------------------------')
 
